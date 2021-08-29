f985f7b69bba884b6eec42a1c3145895b964778d
codymart1986 <codymart1986@hotmail.com>
2021-08-29 15:49:28 -0700

    defined created and made imported to views.py, updated url patterns

--- superheroes/heroes/views.py
+++ superheroes/heroes/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Hero
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -16,3 +18,16 @@ def detail(request, hero_id):
         'single_hero' : single_hero
     }
     return render (request, 'heroes/details.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero = Hero(name=name, alter_ego=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catchphrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('heroes:index'))
+    else:
+        return render(request, 'heroes/create.html')
